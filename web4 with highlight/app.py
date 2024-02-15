53addebeb96d6a089ff3f00675a9d6a68198a9c5
Long hang Zac Chung <com>
2024-02-15 12:03:36 +0000

    1

--- web4 with highlight/app.py
+++ web4 with highlight/app.py
@@ -1,38 +1,55 @@
 import nltk
-# nltk.download('punkt')
+nltk.download('punkt')
 from flask import Flask, render_template, request, jsonify
+
 
 import joblib
 import os
 import re
 import torch
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer,  AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+
+#to use the server
+# run app py
+#
+#disown
+#kill of necessary
+
+#consent form  after demo... ask participants there in the website
+# add flags to lead to other version of the summarisation models.
+#handle error, explain in the website of the errors.
 
 
-
-# # in the ui, allow user to choose which classifier eg nb or bert
-# # highlighting in the ui
-# # try bert for sentence classifiaction as baseline
-# # presentaion, use mockup demos of your ui,
 app = Flask(__name__)
 
 models_dir = os.path.join(os.path.dirname(__file__), 'models')
-
-consp_classifier = joblib.load(os.path.join(models_dir, 'consp_classifier.joblib'))
-tfidf_vectorizer = joblib.load(os.path.join(models_dir, 'tfidf_vectorizer.joblib'))
-
+summaristion_model_dir=os.path.join(os.path.dirname(__file__),"tm-small-cnn-model")
+sum_tokeniser_model_dir=os.path.join(os.path.dirname(__file__),"tokenizer")
 # Load the model and tokenizer
 Bert_classifier = BertForSequenceClassification.from_pretrained(models_dir)
 Bert_tokenizer = BertTokenizer.from_pretrained(models_dir)
 
+summarization_model = AutoModelForSeq2SeqLM.from_pretrained(summaristion_model_dir)
+tokenizer_model = AutoTokenizer.from_pretrained(sum_tokeniser_model_dir)
 
+pipe = pipeline("summarization", model=summarization_model, tokenizer=tokenizer_model)
 
 
 @app.route('/')
 def index():
         return render_template('index.html')
 
-@app.route('/classify', methods=['POST'])
+#render version 1 of the website
+@app.route('/1')
+def index_1():
+    return render_template('index.html', version='1')
+
+#render version 2 of the website which includes summarisation
+@app.route('/2')
+def index_2():
+    return render_template('index.html', version='2')
+
+@app.route('/1/classify', methods=['POST'])
 def classify():
     data = request.get_json()
 
@@ -40,6 +57,19 @@ def classify():
         text = data["text"]
         model = data["model"]
         
+        #temperary summarisation tool
+        
+        
+
+        # Initialize the HuggingFace summarization pipeline
+        # summarizer = pipeline("summarization")
+        # summarized = summarizer(text, min_length=75, max_length=300)
+
+        # # Print summarized text
+        # print(summarized)
+
+
+
         #tokenise and split sentences
         sentences = nltk.sent_tokenize(text)
         num_sentences = 0
@@ -57,12 +87,6 @@ def classify():
                 result = torch.argmax(probs).item()
                 print("bert result is",result)
                 prob = probs[0][result].item()  # Conspiracy probability
-
-            elif model == "sklearn":
-                sentence_tfidf = tfidf_vectorizer.transform([sentence])
-                result = consp_classifier.predict(sentence_tfidf)[0]
-                probresult = consp_classifier.predict_proba(sentence_tfidf)
-                prob = probresult[0][result]  # Conspiracy probability
 
             else:
                 return jsonify({'error': 'Invalid model. Please select either "bert" or "sklearn".'})
@@ -93,5 +117,67 @@ def classify():
     else:
         return jsonify({'error': 'Invalid input format. Please provide the required fields "text" and "model".'})
 
+# versionn 2 of the website, summarises text before classifier
+@app.route('/2/summarize-and-classify', methods=['POST'])
+def summarize_and_classify():
+    data = request.get_json()
+
+    if "text" in data and "model" in data:
+        text = data["text"]
+        model = data["model"]
+
+        # Summarize the text
+        summarized = pipe(text, min_length=75, max_length=300)
+
+        # Get the summary text
+        summary = summarized[0]["summary_text"]
+        # Tokenize and split sentences
+        sentences = nltk.sent_tokenize(summary)
+        num_sentences = 0
+        num_consp_sentences = 0
+        split_sentences = []
+
+        for sentence in sentences:
+            if model == "bert":
+                # Tokenize the input sentence and convert to tensors
+                inputs = Bert_tokenizer(sentence, return_tensors="pt", padding=True, truncation=True, max_length=512)
+
+                # Get the model's predictions
+                outputs = Bert_classifier(**inputs)
+                probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+                result = torch.argmax(probs).item()
+                print("bert result is", result)
+                prob = probs[0][result].item()  # Conspiracy probability
+
+            else:
+                return jsonify({'error': 'Invalid model. Please select either "bert" or "sklearn".'})
+
+            if result == 0 or result == 1:  # Conspiracy classification
+                if result == 1:
+                    num_consp_sentences += 1
+
+                # append sentence, its individual result, and conspiracy probability to sentences list
+                split_sentences.append({sentence: {"classification": int(result), "probability": prob}})
+
+            num_sentences += 1
+
+        conspiracy_percentage = (num_consp_sentences / num_sentences) * 100 if num_sentences > 0 else 0
+        overall_classification = 1 if conspiracy_percentage > 50 else 0
+        print("result", {'overall_classification': overall_classification,
+                         'sentences': split_sentences,
+                         'num_consp_sentences': num_consp_sentences,
+                         'num_sentences': num_sentences,
+                         'conspiracyPercentage': round(conspiracy_percentage, 2)})
+        return jsonify({'overall_classification': overall_classification,
+                        'sentences': split_sentences,
+                        'num_consp_sentences': num_consp_sentences,
+                        'num_sentences': num_sentences,
+                        'conspiracyPercentage': round(conspiracy_percentage, 2),
+                        'summary': summary})
+
+    else:
+        return jsonify({'error': 'Invalid input format. Please provide the required fields "text" and "model".'})
+
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0',port=8090)
--- web4 with highlight/static/script.js
+++ web4 with highlight/static/script.js
@@ -9,9 +9,26 @@ document.addEventListener('DOMContentLoaded', function () {
     textForm.addEventListener('submit', async function (e) {
         e.preventDefault();
 
+        //the links decide which version of the code users are choosing
+        // Get the version of the website from the URL
+        // const urlParams = new URLSearchParams(window.location.search);
+        // const version = urlParams.get('version');
+        const pathname = window.location.pathname; // This will return "/1" or "/2"
+        const version = pathname.split('/')[1];
+
+
+
 
         // selection of model
         const inputText = document.getElementById('inputText').value;
+
+        //handle error for empty input text
+        // Check the input text
+        if (inputText.length === 0) {
+            // Display an alert message if the input text is empty
+            alert('Your input is empty. Please enter some text for classification.');
+            return;
+        }
         // const model = document.getElementById('modelSelection').value;
         const model = "bert"
 
@@ -20,77 +37,184 @@ document.addEventListener('DOMContentLoaded', function () {
             "text": inputText,
             "model": model
         };
+        // Set the route based on the version
+        let route;
+        if (version === '1') {
+            route = '/1/classify'; // call the classify route
 
-        const response = await fetch('/classify', {
-            method: 'POST',
-            body: JSON.stringify(inputData),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+            const response = await fetch(route, {
+                method: 'POST',
+                body: JSON.stringify(inputData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-        try {
-            const data = await response.json();
+            try {
+                const data = await response.json();
 
 
 
-            if (data.num_sentences !== undefined && data.num_consp_sentences !== undefined) {
-                const sentenceInfo = `The text's conspiracy result is ${data.overall_classification === 0 ?
-                    "Non Conspiracy" : "Potential Conspiracy"}. You have ${data.num_consp_sentences} 
-                     Conspiracy Sentences in ${data.num_sentences} sentences, leading to a conspiracy percentage of 
-                     ${data.conspiracyPercentage}%`;
-                sentenceInfoDiv.innerHTML = sentenceInfo;
-                sentenceInfoDiv.style.display = 'block';
+                if (data.num_sentences !== undefined && data.num_consp_sentences !== undefined) {
+                    const sentenceInfo = `The text's conspiracy result is ${data.overall_classification === 0 ?
+                        "Non Conspiracy" : "Potential Conspiracy"}. You have ${data.num_consp_sentences} 
+                         Conspiracy Sentences in ${data.num_sentences} sentences, leading to a conspiracy percentage of 
+                         ${data.conspiracyPercentage}%`;
+                    sentenceInfoDiv.innerHTML = '';
+                    sentenceInfoDiv.innerHTML = sentenceInfo;
+                    sentenceInfoDiv.style.display = 'block';
+                }
+
+                if (data.sentences !== undefined && data.sentences.length > 0) {
+                    userTextInput.innerHTML = '';
+                    userTextInput.innerHTML = `</h4>Here is your classified text\n</h4>`;
+
+                    let combinedText = '';
+                    data.sentences.forEach((sentenceObj, index) => {
+                        const sentence = Object.keys(sentenceObj)[0];
+                        const result = sentenceObj[sentence].classification
+                        const prob = sentenceObj[sentence].probability;
+                        //modify the range of the probabilities.   colourisation       test accuracy
+                        //deal with \n characters \/\/
+
+                        //generate text summarisation and train a model on it
+
+
+
+                        // Calculate the color intensity based on the probability
+                        var intensity = Math.floor(prob * 255);  // Scale the probability to 0-255
+
+                        if (result === 0) {
+                            intensity = Math.floor(127 * (1 - prob))
+
+                        } else {
+                            intensity = Math.floor(127 + 127 * (prob))
+
+                        }
+
+                        // HeuserTextInputat map effect 
+                        // highlight text color based on the classification result
+                        let color;
+                        if (result === 0) {  // Non conspiracy: white for high probability and pink for low probability
+                            color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
+                        } else {  // Potential conspiracy: pink for low probability and red for high probability
+                            color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
+                        }
+
+                        combinedText += `<span style="background-color: ${color};">${sentence}</span> `;
+                    });
+                    userTextInput.innerHTML += combinedText;
+                    userTextInput.style.display = 'block';
+                    userTextInput.style.display = 'block';
+
+                } else {
+                    userTextInput.innerHTML = ' Classification failed No sentences found.';
+                }
+
+            } catch (error) {
+                console.error("Error parsing JSON:", error);
+                resultDiv.innerHTML = 'Classification failed due to a server error.';
             }
 
-            if (data.sentences !== undefined && data.sentences.length > 0) {
-                userTextInput.innerHTML = '';
+        } else if (version === '2') {
 
-                let combinedText = '';
-                data.sentences.forEach((sentenceObj, index) => {
-                    const sentence = Object.keys(sentenceObj)[0];
-                    const result = sentenceObj[sentence].classification
-                    const prob = sentenceObj[sentence].probability;
-                    //modify the range of the probabilities.   colourisation       test accuracy
-                    //deal with \n characters \/\/
+            route = '/2/summarize-and-classify'; // call the summarize-and-classify route
 
-                    //generate text summarisation and train a model on it
+            const response = await fetch(route, {
+                method: 'POST',
+                body: JSON.stringify(inputData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            try {
+                const data = await response.json();
 
 
 
-                    // Calculate the color intensity based on the probability
-                    var intensity = Math.floor(prob * 255);  // Scale the probability to 0-255
+                if (data.num_sentences !== undefined && data.num_consp_sentences !== undefined) {
+                    const sentenceInfo = `The text's conspiracy result is ${data.overall_classification === 0 ?
+                        "Non Conspiracy" : "Potential Conspiracy"}. You have ${data.num_consp_sentences} 
+                         Conspiracy Sentences in ${data.num_sentences} sentences, leading to a conspiracy percentage of 
+                         ${data.conspiracyPercentage}%`;
+                    sentenceInfoDiv.innerHTML = '';
+                    sentenceInfoDiv.innerHTML = sentenceInfo;
+                    sentenceInfoDiv.style.display = 'block';
+                }
 
-                    if (result === 0) {
-                        intensity = Math.floor(127 * (1 - prob))
+                if (data.sentences !== undefined && data.sentences.length > 0) {
+                    userTextInput.innerHTML = '';
+                    userTextInput.innerHTML = `<h4>Here is a summary of your text\n</h4>`;
 
-                    } else {
-                        intensity = Math.floor(127 + 127 * (prob))
+                    let combinedText = '';
+                    data.sentences.forEach((sentenceObj, index) => {
+                        const sentence = Object.keys(sentenceObj)[0];
+                        const result = sentenceObj[sentence].classification
+                        const prob = sentenceObj[sentence].probability;
+                        //modify the range of the probabilities.   colourisation       test accuracy
+                        //deal with \n characters \/\/
 
-                    }
+                        //generate text summarisation and train a model on it
 
-                    // Heat map effect 
-                    // highlight text color based on the classification result
-                    let color;
-                    if (result === 0) {  // Non conspiracy: white for high probability and pink for low probability
-                        color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
-                    } else {  // Potential conspiracy: pink for low probability and red for high probability
-                        color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
-                    }
 
-                    combinedText += `<span style="background-color: ${color};">${sentence}</span> `;
-                });
-                userTextInput.innerHTML = combinedText;
-                userTextDiv.style.display = 'block';
-                userTextInput.style.display = 'block';
 
-            } else {
-                userTextInput.innerHTML = ' Classification failed No sentences found.';
+                        // Calculate the color intensity based on the probability
+                        var intensity = Math.floor(prob * 255);  // Scale the probability to 0-255
+
+                        if (result === 0) {
+                            intensity = Math.floor(127 * (1 - prob))
+
+                        } else {
+                            intensity = Math.floor(127 + 127 * (prob))
+
+                        }
+
+                        // HeuserTextInputat map effect 
+                        // highlight text color based on the classification result
+                        let color;
+                        if (result === 0) {  // Non conspiracy: white for high probability and pink for low probability
+                            color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
+                        } else {  // Potential conspiracy: pink for low probability and red for high probability
+                            color = `rgb(${255}, ${255 - intensity}, ${255 - intensity})`;
+                        }
+
+                        combinedText += `<span style="background-color: ${color};">${sentence}</span> `;
+                    });
+                    userTextInput.innerHTML += combinedText;
+                    userTextInput.style.display = 'block';
+                    userTextInput.style.display = 'block';
+
+                } else {
+                    userTextInput.innerHTML = ' Classification failed No sentences found.';
+                }
+
+            } catch (error) {
+                console.error("Error parsing JSON:", error);
+                resultDiv.innerHTML = 'Classification failed due to a server error.';
             }
 
-        } catch (error) {
-            console.error("Error parsing JSON:", error);
-            resultDiv.innerHTML = 'Classification failed due to a server error.';
+
+
+
+
+
+
+
+
+
+
+        } else {
+            // case for missing version
+            alert('Your version is Invalid or missing. Please enter a valid verison int he URL as provided');
+
+
+
+
+
+            return;
+
         }
+
     });
 });
